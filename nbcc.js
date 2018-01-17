f3d05a2f09ac61727d8f3fb77a5e32c384c0596e
Axel Messinese <axel.messinese@epfl.ch>
2018-01-17 15:59:58 +0100

    update

--- nbcc.js
+++ nbcc.js
@@ -22,7 +22,20 @@ define(['require', 'jquery', 'base/js/utils', 'base/js/dialog', 'base/js/events'
 
             var requirement = IPython.notebook.metadata.requirements
 
-            var python_code = "print 'hello zorld'"
+            var python_code = "import pkg_resources; from pkg_resources import DistributionNotFound, VersionConflict import json"+
+                              "requirements="+JSON.stringify(requirement)+":\; "+
+                              "def test_requirement(requirement): "+
+                               "   try: "+
+                                "      pkg_resources.require(requirement) "+
+                                 " except DistributionNotFound as err: "+
+                                  "    return err.req.name, err.report()"+
+                                  " except VersionConflict as err: "+
+                                     " return err.req.name, err.report()"+
+                              " checks = {}; "+
+                              " for requirement in requirements: "+
+                              "     check = test_requirement(requirement) "+
+                              "    if check: checks[check[0]] = check[1]"+
+                              " print json.dumps(checks)"
             
             kernel.execute(python_code,{ iopub : 
                 { output : data => handle(JSON.parse(data.content.text))}

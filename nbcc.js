7b21104156c399f9f70332d456a5438d5b64b9a8
Axel Messinese <axel.messinese@epfl.ch>
2018-01-17 11:38:59 +0100

    first commit

---
+++ nbcc.js
@@ -0,0 +1,23 @@
+define( function () {
+    // change the compatibility icon color based on the compatibility checks
+    var handle = function(requirementChecks) {
+        const color = Object.keys(requirementChecks).length ? "red" : "green";
+        $('#compatibility').css('color', color);
+    }
+    
+    var compatibility_check_icon = function () {
+        // add compatibility icon if necessary
+        if ($('#compatibility').length === 0) {
+            $('#notification_area').append('<i id="compatibility" class="kernel_busy_icon" title="Kernel Idle"></i>');
+        }
+    };
+    
+    var load_ipython_extension = function () {
+        compatibility_check_icon();
+    };
+    
+    return {
+        load_ipython_extension : load_ipython_extension,
+    };
+    
+});

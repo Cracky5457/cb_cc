dd39d85b09596804f388cd9d536f2983b0621323
Axel Messinese <axel.messinese@epfl.ch>
2018-01-17 11:46:21 +0100

    update

--- nbcc.js
+++ nbcc.js
@@ -3,9 +3,10 @@ define( function () {
     var handle = function(requirementChecks) {
         const color = Object.keys(requirementChecks).length ? "red" : "green";
         $('#compatibility').css('color', color);
-    }
+    };
     
     var compatibility_check_icon = function () {
+        console.log("load compatibility icon")
         // add compatibility icon if necessary
         if ($('#compatibility').length === 0) {
             $('#notification_area').append('<i id="compatibility" class="kernel_busy_icon" title="Kernel Idle"></i>');
